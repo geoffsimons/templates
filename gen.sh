b0833c392b6b970fc193de6d6ec0bed5704dd0b3
Geoff Simons <geoff.simons@gmail.com>
2016-12-13 13:53:28 -0800

    Added commands to init

--- gen.sh
+++ gen.sh
@@ -4,3 +4,16 @@ cp .gitignore $1/.gitignore
 cp README.md $1/README.md
 cp gulpfile.js $1/gulpfile.js
 cp package.json $1/package.json
+
+# change to the new project root
+cd $1
+
+# setup git, script assumes new project
+git init
+
+# Install node dependencies
+npm i
+
+# Consider git add . && git commit -m "Initial commit"
+# Should that happen on a branch, or maybe this is something that
+# can happen on master.
